31adc15b3384c29902c171ce09b2748cd2f08e5d
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-06-25 01:30:34 +0100

    chore(play): playtest `DialogueUI`

--- scenes/core/main_game.gd
+++ scenes/core/main_game.gd
@@ -1,8 +1,17 @@
 extends Control
 
 @onready var visual_layer: Control = $VisualLayer
+@onready var dialogue_ui = $UILayer/DialogueUI
+
 
 func _ready() -> void:
 	# For testing purposes, use dispersion event cg.
 	var test_asset_path: String = "res://assets/cgs/cg_ch3_prism_dispersion.png"
 	visual_layer.display_scene(visual_layer.VisualType.EVENT_CG, test_asset_path)
+	
+	var zach_font_color = Color("#003D7A")
+	var zach_bg_color = Color("#99CCFF")
+	dialogue_ui.update_speaker("Zach", zach_font_color, zach_bg_color)
+	
+	var physics_line = "When white light passes through a prism, it separates into a spectrum of colors. This is known as [color=#FF7F00][url=dispersion_def]dispersion[/url][/color]."
+	dialogue_ui.update_dialogue(physics_line)
