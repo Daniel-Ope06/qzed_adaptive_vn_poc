713e02cc49a7f255fd17305bcafa8b936e074025
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-06-25 01:30:06 +0100

    feat(ui): define logic to update speaker name and dialogue

--- scenes/core/dialogue_ui.gd
+++ scenes/core/dialogue_ui.gd
@@ -24,13 +24,14 @@ func _ready() -> void:
 	
 	# Connect Signals
 	hide_ui_button.pressed.connect(_on_hide_ui_button_pressed)
+	dialogue_text.meta_clicked.connect(_on_keyword_clicked)
 
 
 # --- HIDE / SHOW UI LOGIC ---
 func _on_hide_ui_button_pressed() -> void:
 	_toggle_ui(false)
 
-# In visual novels, tapping anywhere on the screen brings the hidden UI back
+# Tapping anywhere on the screen brings the hidden UI back
 func _input(event: InputEvent) -> void:
 	if is_ui_hidden:
 		# Detects a left-click on PC or a screen tap on Mobile
@@ -45,3 +46,29 @@ func _toggle_ui(is_visible: bool) -> void:
 	dialogue_alignment.visible = is_visible
 	button_alignment.visible = is_visible
 	is_ui_hidden = !is_visible
+
+
+# --- UPDATE SPEAKER NAME ---
+func update_speaker(speaker: String, text_color: Color, bg_color: Color) -> void:
+	speaker_name.text = speaker
+	speaker_name.add_theme_color_override("default_color", text_color)
+	
+	# Duplicate and inject the color into the NamePanel
+	var panel_style = name_panel.get_theme_stylebox("panel").duplicate()
+	panel_style.bg_color = bg_color
+	name_panel.add_theme_stylebox_override("panel", panel_style)
+	
+	# Duplicate and inject the color into your puzzle piece tab
+	if tab_piece and tab_piece.has_theme_stylebox("panel"):
+		var tab_style = tab_piece.get_theme_stylebox("panel").duplicate()
+		tab_style.bg_color = bg_color
+		tab_piece.add_theme_stylebox_override("panel", tab_style)
+
+
+# --- UPDATE DIALOGUE & KEYWORDS ---
+func update_dialogue(text: String) -> void:
+	dialogue_text.text = text
+
+func _on_keyword_clicked(meta_data) -> void:
+	print("Keyword clicked! Loading dictionary entry for: ", meta_data)
+	# TODO: Dictionary UI here based on the meta_data
