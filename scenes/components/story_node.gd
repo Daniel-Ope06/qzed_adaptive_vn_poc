fc2ac7c3cd3580bda8f331b4ccf0913345c6de2b
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-07-16 05:24:12 +0100

    feat(ui): set up story node states

---
+++ scenes/components/story_node.gd
@@ -0,0 +1,46 @@
+extends PanelContainer
+class_name StoryNode
+
+enum NodeState { LOCKED, UNLOCKED, COMPLETED }
+
+const ICON_LOCK = preload("res://assets/ui_icons/icon_tl_lock.png")
+const ICON_CHECK = preload("res://assets/ui_icons/icon_tl_check.png")
+const COLOR_LOCKED: Color = Color("#D6D6D6") # Gray
+const COLOR_UNLOCKED: Color = Color("#0070E0") # Blue
+
+@onready var node_image: TextureRect = $ImageMask/Background
+@onready var node_icon: TextureRect = $StateIcon
+@onready var node_title: Label = $TitleBox/Title
+
+var current_state: NodeState = NodeState.LOCKED
+var panel_style: StyleBoxFlat
+
+func _ready() -> void:
+	panel_style = get_theme_stylebox("panel").duplicate()
+	add_theme_stylebox_override("panel", panel_style)
+
+func setup_node(title: String, image: Texture2D, state: NodeState = NodeState.LOCKED) -> void:
+	node_title.text = title
+	node_image.texture = image
+	set_state(state)
+
+func set_state(new_state: NodeState) -> void:
+	current_state = new_state
+	
+	match current_state:
+		NodeState.LOCKED:
+			node_icon.texture = ICON_LOCK
+			node_icon.show()
+			node_image.modulate.a = 0.4
+			panel_style.border_color = COLOR_LOCKED
+			
+		NodeState.UNLOCKED:
+			node_icon.hide()
+			node_image.modulate.a = 1
+			panel_style.border_color = COLOR_UNLOCKED
+			
+		NodeState.COMPLETED:
+			node_icon.texture = ICON_CHECK
+			node_icon.show()
+			node_image.modulate.a = 0.8
+			panel_style.border_color = COLOR_UNLOCKED
