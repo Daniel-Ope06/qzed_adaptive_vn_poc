f1bcbfc5c651879052e2589f42ea049f2ddaa63e
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-07-17 10:10:31 +0100

    feat(ui): define `CHAR_STYLES`

--- scenes/core/main_game.gd
+++ scenes/core/main_game.gd
@@ -1,17 +1,32 @@
 extends Control
 
+const CHAR_STYLES = {
+	"Quinn": {
+		"bg_color": Color("#F57A00"), 
+		"font_color": Color("#FFF5EB")
+	},
+	"Zach": {
+		"bg_color": Color("#99CCFF"), 
+		"font_color": Color("#003D7A")
+	},
+	"Ella": {
+		"bg_color": Color("#fd6969"), 
+		"font_color": Color("#7d2d16")
+	},
+	"Daniel": {
+		"bg_color": Color("#428e2c"), 
+		"font_color": Color("#74c84f")
+	},
+	# A fallback for a narrator or an unknown speaker
+	"System": {
+		"bg_color": Color("#525252"),
+		"font_color": Color("#F5F5F5")
+	}
+}
+
 @onready var visual_layer: Control = $VisualLayer
 @onready var dialogue_ui = $UILayer/DialogueUI
 
 
 func _ready() -> void:
-	# For testing purposes, use dispersion event cg.
-	var test_asset_path: String = "res://assets/cgs/cg_ch3_prism_dispersion.png"
-	visual_layer.display_scene(visual_layer.VisualType.EVENT_CG, test_asset_path)
-	
-	var zach_font_color = Color("#003D7A")
-	var zach_bg_color = Color("#99CCFF")
-	dialogue_ui.update_speaker("Zach", zach_font_color, zach_bg_color)
-	
-	var physics_line = "When white light passes through a prism, it separates into a spectrum of colors. This is known as [color=#FF7F00][url=dispersion_def]dispersion[/url][/color]."
-	dialogue_ui.update_dialogue(physics_line)
+	pass
