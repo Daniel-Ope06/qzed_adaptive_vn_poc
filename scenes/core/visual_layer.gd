d43ac916bb9f27b9f48c0720a9605ad5f732b315
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-07-17 08:36:35 +0100

    refactor(core): rename visual types

--- scenes/core/visual_layer.gd
+++ scenes/core/visual_layer.gd
@@ -1,35 +1,28 @@
 extends Control
 
 enum VisualType {
-	EVENT_CG,
-	STANDARD_DIALOGUE,
-	PANORAMIC_CG
+	STATIC_CG,
+	ANIMATED_CG,
+	DIALOGUE_CG
 }
 
 @onready var background: TextureRect = $Background
 
 func display_scene(type: VisualType, asset_path: String) -> void:
 	match type:
-		VisualType.EVENT_CG:
-			_show_event_cg(asset_path)
-		VisualType.STANDARD_DIALOGUE:
-			pass # Add sprite handling here next
-		VisualType.PANORAMIC_CG:
-			pass # Add the Camera2D pan logic here later
+		VisualType.STATIC_CG:
+			_show_static_cg(asset_path)
+		VisualType.ANIMATED_CG:
+			pass 
+		VisualType.DIALOGUE_CG:
+			pass # Add sprite handling
 
-func _show_event_cg(cg_path: String) -> void:
+func _show_static_cg(cg_path: String) -> void:
 	# Load the full-color illustration
 	var texture = load(cg_path)
 	if texture:
 		background.texture = texture
 	else:
 		push_error("Visual Layer: Could not load Event CG at " + cg_path)
-	
 	# Ensure the image is static and centered
 	background.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
-	
-	# Reset any Panoramic camera movement (to be implemented)
-	# $Camera2D.position = Vector2.ZERO
-	
-	# Hide character sprites, as Event CGs are unified shots (to be implemented)
-	# $SpriteContainer.hide()
