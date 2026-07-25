ca6bd17271ffbf965dce3e927aba8ade2ff5ecd8
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-07-25 02:14:21 +0100

    refactor(core): make `flowchart_data` global

--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -1,3 +1,86 @@
 extends Node
 
 var current_story_id: String = ""
+
+var flowchart_data = [
+	{
+		"size": "one",
+		"type": "story",
+		"story_id": "",
+		"title": "Prologue",
+		"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
+		"state": StoryNode.NodeState.LOCKED
+	},
+	{
+		"size": "one",
+		"type": "chapter",
+		"title": "THE RESCUE",
+		"subtitle": "Reflection & Refraction",
+		"state": ChapterNode.NodeState.UNLOCKED
+	},
+	{
+		"size": "many",
+		"nodes": [
+			{
+				"type": "story",
+				"story_id": "chapter_1a",
+				"title": "Chapter 1A", 
+				"image": "res://assets/cg_static/quinn_trapped.webp",
+				"state": StoryNode.NodeState.UNLOCKED
+			},
+			{
+				"type": "story",
+				"story_id": "",
+				"title": "Chapter 1B", 
+				"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
+				"state": StoryNode.NodeState.LOCKED
+			}
+		]
+	},
+	{
+		"size": "one",
+		"type": "quiz",
+		"title": "Q1",
+		"state": QuizNode.NodeState.LOCKED
+	},
+	{
+		"size": "one",
+		"type": "chapter",
+		"title": "CHAPTER 2",
+		"subtitle": "Dispersion",
+		"state": ChapterNode.NodeState.LOCKED
+	},
+	{
+		"size": "many",
+		"nodes": [
+			{
+				"type": "story",
+				"story_id": "",
+				"title": "Chapter 2A", 
+				"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
+				"state": StoryNode.NodeState.LOCKED
+			},
+			{
+				"type": "story",
+				"story_id": "",
+				"title": "Chapter 2B", 
+				"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
+				"state": StoryNode.NodeState.LOCKED
+			}
+		]
+	},
+	{
+		"size": "one",
+		"type": "quiz",
+		"title": "Q2",
+		"state": QuizNode.NodeState.LOCKED
+	},
+	{
+		"size": "one",
+		"type": "story",
+		"story_id": "",
+		"title": "Epilogue",
+		"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
+		"state": StoryNode.NodeState.LOCKED
+	},
+]
--- scenes/menus/timeline.gd
+++ scenes/menus/timeline.gd
@@ -10,121 +10,6 @@ const CHAPTER_NODE = preload("res://scenes/components/chapter_node.tscn")
 const COLOR_LINE_ACTIVE = Color("#0070E0") # Blue
 const COLOR_LINE_INACTIVE = Color("#D6D6D6") # Grey
 
-var flowchart_data = [
-	{
-		"size": "one",
-		"type": "story",
-		"story_id": "",
-		"title": "Prologue",
-		"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
-		"state": StoryNode.NodeState.LOCKED
-	},
-	{
-		"size": "one",
-		"type": "chapter",
-		"title": "THE RESCUE",
-		"subtitle": "Reflection & Refraction",
-		"state": ChapterNode.NodeState.UNLOCKED
-	},
-	{
-		"size": "many",
-		"nodes": [
-			{
-				"type": "story",
-				"story_id": "chapter_1a",
-				"title": "Chapter 1A", 
-				"image": "res://assets/cg_static/quinn_trapped.webp",
-				"state": StoryNode.NodeState.UNLOCKED
-			},
-			{
-				"type": "story",
-				"story_id": "",
-				"title": "Chapter 1B", 
-				"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
-				"state": StoryNode.NodeState.LOCKED
-			}
-		]
-	},
-	{
-		"size": "one",
-		"type": "quiz",
-		"title": "Q1",
-		"state": QuizNode.NodeState.LOCKED
-	},
-	{
-		"size": "one",
-		"type": "chapter",
-		"title": "CHAPTER 2",
-		"subtitle": "Total Internal Reflection",
-		"state": ChapterNode.NodeState.LOCKED
-	},
-	{
-		"size": "many",
-		"nodes": [
-			{
-				"type": "story",
-				"story_id": "",
-				"title": "Chapter 2A", 
-				"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
-				"state": StoryNode.NodeState.LOCKED
-			},
-			{
-				"type": "story",
-				"story_id": "",
-				"title": "Chapter 2B", 
-				"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
-				"state": StoryNode.NodeState.LOCKED
-			}
-		]
-	},
-	{
-		"size": "one",
-		"type": "quiz",
-		"title": "Q2",
-		"state": QuizNode.NodeState.LOCKED
-	},
-	{
-		"size": "one",
-		"type": "chapter",
-		"title": "CHAPTER 3",
-		"subtitle": "Dispersion",
-		"state": ChapterNode.NodeState.LOCKED
-	},
-	{
-		"size": "many",
-		"nodes": [
-			{
-				"type": "story",
-				"story_id": "",
-				"title": "Chapter 3A", 
-				"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
-				"state": StoryNode.NodeState.LOCKED
-			},
-			{
-				"type": "story",
-				"story_id": "",
-				"title": "Chapter 3B", 
-				"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
-				"state": StoryNode.NodeState.LOCKED
-			}
-		]
-	},
-	{
-		"size": "one",
-		"type": "quiz",
-		"title": "Q3",
-		"state": QuizNode.NodeState.LOCKED
-	},
-	{
-		"size": "one",
-		"type": "story",
-		"story_id": "",
-		"title": "Epilogue",
-		"image": "res://assets/cgs/cg_ch3_prism_dispersion.png",
-		"state": StoryNode.NodeState.LOCKED
-	},
-]
-
 var tracked_nodes: Array = []
 
 func _ready() -> void:
@@ -133,7 +18,7 @@ func _ready() -> void:
 	draw_connections()
 
 func build_timeline() -> void:
-	for entity in flowchart_data:
+	for entity in GameManager.flowchart_data:
 		var entity_nodes: Array = []
 		
 		if entity["size"] == "one":
